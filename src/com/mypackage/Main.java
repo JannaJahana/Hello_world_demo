d5350a735488b935de04b4be8a7ca5fd29599c41
Janna Jahana <jannajahana@gmail.com>
2021-08-23 17:30:16 +0530

    Initial commit

---
+++ src/com/mypackage/Main.java
@@ -0,0 +1,10 @@
+package com.mypackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Janna";
+        System.out.println("Hello "+name);
+    }
+}
