1d4f698018e98a8d4ffa115d36b7922415b3b016
JannaJahana <89299799+JannaJahana@users.noreply.github.com>
2021-08-23 19:16:42 +0530

    Update Greeting.java

--- src/com/mypackage/Greeting.java
+++ src/com/mypackage/Greeting.java
@@ -2,6 +2,7 @@ package com.mypackage;
 
 public class Greeting {
     public void greet(){
+        System.out.println("You look beautiful");
         System.out.println("Have a nice day");
     }
 }
