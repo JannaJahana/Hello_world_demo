2f38e9bf0f472ac8ef7b9e7366b80c547e7e9deb
krishitha <kstalin9@gmail.com>
2021-08-27 15:36:01 +0530

    updated janna with janna jahana

--- src/com/mypackage/Main.java
+++ src/com/mypackage/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name = "Janna";
+        String name = "Janna jahana";
         System.out.println("Hello "+name);
 	    Greeting msg = new Greeting();
 	    msg.greet();
